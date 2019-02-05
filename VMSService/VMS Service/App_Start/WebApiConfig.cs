a5fdd703fb205eb9de7676f737b22f3c3787db44
balab <balamurugan.b@honeywell.com>
2019-02-05 11:41:39 +0530

    andriod mobile app using andriod studio

--- VMSService/VMS Service/App_Start/WebApiConfig.cs
+++ VMSService/VMS Service/App_Start/WebApiConfig.cs
@@ -14,11 +14,11 @@ namespace VMS_Service
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
 
             config.Routes.MapHttpRoute(
                 name: "ActionApi",
--- VMSService/VMS Service/Controllers/LoginController.cs
+++ VMSService/VMS Service/Controllers/LoginController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using VMS_Service.Models;
 
 namespace VMS_Service.Controllers
